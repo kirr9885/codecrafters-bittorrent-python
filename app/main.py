0333bcc46ef80f12a01b62e03b8607ee83b668f5
Kiran Poudel <kirr9885@gmail.com>
2024-07-30 19:30:07 +0545

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,69 +1,53 @@
+import hashlib
 import json
 import sys
-import bencodepy
-import hashlib
 
+import bencodepy
+
+# import requests - available if you need it!
+
+bc = bencodepy.Bencode(encoding="utf-8")
+
+
+# Examples:
+#
+# - decode_bencode(b"5:hello") -> b"hello"
+# - decode_bencode(b"10:hello12345") -> b"hello12345"
 def decode_bencode(bencoded_value):
-    first_char = chr(bencoded_value[0])
-    if first_char.isdigit():
-        length_end = bencoded_value.find(b":")
-        length = int(bencoded_value[:length_end])
-        start = length_end + 1
-        return bencoded_value[start:start + length]
-    elif first_char == "i":
-        end = bencoded_value.find(b"e")
-        return int(bencoded_value[1:end])
-    elif first_char == "l" or first_char == "d":
-        return bencodepy.decode(bencoded_value)
-    else:
-        raise NotImplementedError("Only strings, integers, lists, and dictionaries are supported at the moment")
+    return bc.decode(bencoded_value)
+
 
 def main():
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <command> <value>")
-        return
-    
     command = sys.argv[1]
-    
-    def bytes_to_str(data):
-        if isinstance(data, bytes):
-            return data.decode("utf-8", errors="replace")
-        elif isinstance(data, int):
-            return data
-        elif isinstance(data, list):
-            return [bytes_to_str(item) for item in data]
-        elif isinstance(data, dict):
-            return {
-                bytes_to_str(key): bytes_to_str(value) for key, value in data.items()
-            }
-        else:
-            raise TypeError(f"Type not serializable: {type(data)}")
-    
+
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
-        decoded_value = decode_bencode(bencoded_value)
-        print(json.dumps(decoded_value, default=bytes_to_str))
-    
+
+        # json.dumps() can't handle bytes, but bencoded "strings" need to be
+        # bytestrings since they might contain non utf-8 characters.
+        #
+        # Let's convert them to strings for printing to the console.
+        def bytes_to_str(data):
+            if isinstance(data, bytes):
+                return data.decode()
+
+            raise TypeError(f"Type not serializable: {type(data)}")
+
+        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     elif command == "info":
-        file_path = sys.argv[2]
-        with open(file_path, "rb") as file:
-            content = file.read()
-        
-        decoded_content = bencodepy.decode(content)
-        data = bytes_to_str(decoded_content)
-        
-        print(f'Tracker URL: {data["announce"]}')
-        print(f'Length: {data["info"]["length"]}')
-        info_hash = hashlib.sha1(bencodepy.encode(decoded_content[b"info"])).hexdigest()
-        print(f'Info Hash: {info_hash}')
-        print(f'Piece Length: {data["info"]["piece length"]}')
-        print("Piece Hashes: ")
-        
-        for i in range(0, len(decoded_content[b"info"][b"pieces"]), 20):
-            print(decoded_content[b"info"][b"pieces"][i : i + 20].hex())
-    
+        with open(sys.argv[2], "rb") as torrent_file:
+            info = torrent_file.read()
+
+        info_dict = bencodepy.Bencode().decode(info)
+        info_hash = hashlib.sha1(bencodepy.encode(info_dict[b"info"]))
+
+        print(f'Tracker URL: {info_dict[b"announce"].decode()}')
+        print(f'Length: {info_dict[b"info"][b"length"]}')
+        print(f"Info Hash: {info_hash.hexdigest()}")
+
     else:
         raise NotImplementedError(f"Unknown command {command}")
+
 
 if __name__ == "__main__":
     main()
